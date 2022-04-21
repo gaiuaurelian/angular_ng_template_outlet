707a9b58aed569cfba5e9ba2133c2bdfeabb7460
gaiuaurelian <aurelian.gaiu11@gmail.com>
2022-04-21 09:29:07 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ interface HumanBestFriend {
   template?: TemplateRef<any>;
   context: {
     description: string;
-    breeds: string[];
+    breeds: { name: string; image: string }[];
   };
 }
 
@@ -66,14 +66,46 @@ export class AppComponent implements OnInit, AfterViewInit {
         key: 'Dogs',
         context: {
           description: 'Breeds of dogs',
-          breeds: ['German Shepard', 'Corgi', 'Fox Terrier'],
+          breeds: [
+            {
+              name: 'German Shepard',
+              image:
+                'https://th.bing.com/th/id/R.c2fcb99c7aad613b995442c21823bd7e?rik=8hF4DISV9UzXXw&pid=ImgRaw&r=0',
+            },
+            {
+              name: 'Corgi',
+              image:
+                'https://th.bing.com/th/id/R.49fc9f797a6f4c773ed8e63543dc7ba2?rik=wTO1x71kcIvEEg&riu=http%3a%2f%2f4.bp.blogspot.com%2f-ZtI8ADZmxPg%2fTpmD-tg6iII%2fAAAAAAAAH2o%2fd84ckw4xIU4%2fs1600%2fpembroke-welsh-corgi-pictures5.JPG&ehk=tKReOcseGSPRuKsgHD7%2f7L5aAd8F4jQc%2bL5xjEPls%2bI%3d&risl=&pid=ImgRaw&r=0',
+            },
+            {
+              name: 'Jack Russell',
+              image:
+                'https://th.bing.com/th/id/R.213e30f1611ecadb03eff10b4db7c72e?rik=L3MRRzzStvMXkQ&pid=ImgRaw&r=0',
+            },
+          ],
         },
       },
       {
         key: 'Cats',
         context: {
           description: 'Breeds of cats',
-          breeds: ['Birman', 'Siamese', 'Sphynx'],
+          breeds: [
+            {
+              name: 'Birman',
+              image:
+                'https://th.bing.com/th/id/OIP.DwHZfV5FE1GdfdPerMdbEQHaFE?w=255&h=180&c=7&r=0&o=5&pid=1.7',
+            },
+            {
+              name: 'Siamese',
+              image:
+                'https://www.photos-public-domain.com/wp-content/uploads/2010/12/siamese_cat.jpg',
+            },
+            {
+              name: 'Sphynx',
+              image:
+                'https://th.bing.com/th/id/OIP.9pyFTIUTkSf2Hx40MS4CRwHaE8?pid=ImgDet&rs=1',
+            },
+          ],
         },
       },
     ];
